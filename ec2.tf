4c9d94e599e397836d29eac5ffa9b9bff6cf8f1e
Thales M Soares <thales.maschio@zup.com.br>
2023-12-01 16:59:00 -0300

    Provision subnet for EC2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "networkstudy-m1" {
     ami = "ami-0fa1ca9559f1892ec"
     instance_type = "t1.micro"
+    subnet_id = aws_subnet.thms-pub-subnet1.id
 
     tags = {
         Name = "networkstudy-m1"
--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,16 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_vpc" "thms-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "networkstudy-one"
+  }
+}
+
+resource "aws_subnet" "thms-pub-subnet1" {
+  vpc_id     = aws_vpc.thms-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "networkstudy-pub-subnet1"
   }
 }
