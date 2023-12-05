f80ca4b94372b07052e2ed25e9715f0af306db94
Thales M Soares <thales.maschio@zup.com.br>
2023-12-05 10:05:51 -0300

    Launch EC2 instance with private IP

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,17 @@
 resource "aws_instance" "networkstudy-m1" {
     ami = "ami-0fa1ca9559f1892ec"
     instance_type = "t1.micro"
-    subnet_id = aws_subnet.thms-pub-subnet1.id
+
+    network_interface {
+        network_interface_id = aws_network_interface.thms-eni.id
+        device_index = 0
+    }
+
+    root_block_device {
+        volume_type           = "gp2"
+        delete_on_termination = true
+        volume_size           = 8  # Minimal size in GiB
+    }
 
     tags = {
         Name = "networkstudy-m1"
--- vpc.tf
+++ vpc.tf
@@ -14,3 +14,38 @@ resource "aws_subnet" "thms-pub-subnet1" {
     Name = "networkstudy-pub-subnet1"
   }
 }
+
+resource "aws_internet_gateway" "thms-igw" {
+  vpc_id = aws_vpc.thms-vpc.id
+
+  tags = {
+    Name = "networkstudy-igw1"
+  }
+}
+
+resource "aws_route_table" "thms-route-table" {
+  vpc_id = aws_vpc.thms-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.thms-igw.id
+  }
+
+  tags = {
+    Name = "networkstudy-rt1"
+  }
+}
+
+resource "aws_route_table_association" "thms-rta-subnet1" {
+  subnet_id      = aws_subnet.thms-pub-subnet1.id
+  route_table_id = aws_route_table.thms-route-table.id
+}
+
+resource "aws_network_interface" "thms-eni" {
+  subnet_id   = aws_subnet.thms-pub-subnet1.id
+  private_ips = ["10.0.1.10"] 
+
+  tags = {
+    Name = "networkstudy-eni"
+  }
+}
